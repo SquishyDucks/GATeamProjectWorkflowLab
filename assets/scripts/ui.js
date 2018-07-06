f1169f0ad808b33976a9603cc2e3f080fecbbadb
tazpowell <hellotazuko@gmail.com>
2018-07-06 11:35:37 -0400

    Connect handlebars file to getBooksSuccess
    - Update development url to heroku link
    - Clear .books div every time getBooksSuccess runs

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,6 +1,8 @@
+const showBooksTemplate = require('./templates/books.handlebars')
 
 const getBooksSuccess = (data) => {
   console.log(data)
+  $('.books').html('')
   const showBooksHtml = showBooksTemplate({ books: data.books })
   $('.books').append(showBooksHtml)
 }
